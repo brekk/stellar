af5cda9728e36a9795334a7276a8f934823b948c
brekk <brekk@brekkbockrath.com>
2024-09-08 17:39:25 -1000

    more

--- tools/excavator.mjs
+++ tools/excavator.mjs
@@ -2,6 +2,7 @@
 
 import { unified } from "unified";
 import slug from "slug";
+import * as prettier from "prettier";
 import remarkObsidian from "remark-obsidian";
 import remarkParse from "remark-parse";
 import rehypeRaw from "rehype-raw";
@@ -35,42 +36,44 @@ const pickaxe = (x) =>
     .use(remarkObsidian)
     .use(remarkFrontmatter, ["yaml"])
     .use(remarkParseFrontmatter)
-    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(remarkRehype) // , { allowDangerousHtml: true })
     .use(rehypeRaw)
     .use(rehypeHighlight)
     .use(rehypeFormat)
-    .use(rehypeStringify)
+    .use(rehypeStringify, { entities: { useNamedReferences: true } })
     //.use(rehypeReact, PROD)
     .process(x);
 
 const cleanName = pipe((x) => x.slice(x.lastIndexOf("/"), -3), slug);
 
 const indentNewlines = replace("\n", "\n    ");
+const format = (x) => prettier.format(x, { semi: false, parser: "typescript" });
+const prettify = encaseP(format);
+const spotFix = replace(/\>\>/g, ">&gt;");
 
 const jsxify = curry((name, raw) => {
   return `import blem from "blem"
 
-/**
- * This file was automatically generated from:
- * ${name}
- */ 
+// This file was automatically generated from:
+// ${name}
 
 export const NAME = "${cleanName(name)}"
 export const DATA = ${stringifyFrontmatter(raw)}
 export const COMPONENT = () => {
   const bem = blem(NAME)
-  return (<article className={bem("")}>
-  ${indentNewlines(raw.value)}
-</article>)
+  return (<article className={bem("")}>${raw.value}</article>)
 `;
 });
+
+//const spotFix = replace(/^>/g, "&gt;");
 
 const readObsidian = (raw) =>
   pipe(
     readFile,
+    //map(spotFix),
     chain(encaseP(pickaxe)),
-
     map(jsxify(raw)),
+    //chain(prettify),
   )(raw);
 
 const j2 = (x) => JSON.stringify(x, null, 2);
