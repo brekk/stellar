22231eb3988d6dfa168a90412838f26ecccd008a
brekk <brekk@brekkbockrath.com>
2024-09-12 15:50:36 -1000

    fix fonts on pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
-//import Layout from '@/app/layout'
+import { Fira_Code, Antonio } from "next/font/google";
+import "@/Root.scss";
+
+const fontAntonio = Antonio({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-antonio",
+});
+
+const fontFiraCode = Fira_Code({
+  subsets: ["latin"],
+  display: "swap",
+  weight: "400",
+  variable: "--font-fira",
+});
 import type { AppProps } from "next/app";
 import "@/Root.scss";
-import "@/HowTo.scss"
+import "@/HowTo.scss";
 import blem from "blem";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const bem = blem("HowToGuide")
+  const bem = blem("HowToGuide");
   return (
-    <main className={bem("")}>
+    <main
+      className={`${bem("")} ${fontAntonio.variable} ${fontFiraCode.variable} ${fontAntonio.className} ${fontFiraCode.className}`}
+    >
       <Component {...pageProps} />
     </main>
-  )
+  );
 }
