83014e4d2ea0512ea12c8ace1e18a0eae912029d
brekk <brekk@brekkbockrath.com>
2024-10-29 21:47:25 -1000

    reworking the excavator tool. currently broken

--- tools/excavator.mjs
+++ tools/excavator.mjs
@@ -1,57 +1,64 @@
 #!/usr/bin/env node
 
-import { nodeTypes } from "@mdx-js/mdx";
-import { unified } from "unified";
-import slug from "slug";
-import * as prettier from "prettier";
-import remarkObsidian from "remark-obsidian";
-import remarkParse from "remark-parse";
-import rehypeRaw from "rehype-raw";
+import { nodeTypes } from "@mdx-js/mdx"
+import { unified } from "unified"
+import { remark } from "remark"
+import slug from "slug"
+import * as prettier from "prettier"
+//import remarkObsidian from "@thecae/remark-obsidian"
+import remarkObsidian from "remark-parse-obsidian"
+import remarkParse from "remark-parse"
+import rehypeRaw from "rehype-raw"
 // import remarkUTF8 from "remark-utf8";
-import rehypeHighlight from "rehype-highlight";
-import rehypeFormat from "rehype-format";
-// import remarkGfm from "remark-gfm";
+import rehypeHighlight from "rehype-highlight"
+import remarkLinks from "remark-wiki-link-plus"
+import rehypeFormat from "rehype-format"
+import remarkBreaks from "remark-breaks"
+import remarkGfm from "remark-gfm"
 //import remarkStringify from "remark-stringify";
-import rehypeStringify from "rehype-stringify";
-import remarkRehype from "remark-rehype";
-import remarkFrontmatter from "remark-frontmatter";
+import rehypeStringify from "rehype-stringify"
+import remarkRehype from "remark-rehype"
+import remarkFrontmatter from "remark-frontmatter"
 //import remarkMDXFrontmatter from "remark-mdx-frontmatter";
-import remarkParseFrontmatter from "remark-parse-frontmatter";
-import remarkReact from "remark-react";
-import fs from "node:fs";
-import { node, encaseP, chain, map, fork } from "fluture";
-import { replace, path, head, slice, pipe, curry } from "ramda";
-import * as PROD from "react/jsx-runtime";
-import rehypeReact from "rehype-react";
+import remarkParseFrontmatter from "remark-parse-frontmatter"
+import remarkReact from "remark-react"
+import fs from "node:fs"
+import { node, encaseP, chain, map, fork } from "fluture"
+import { replace, path, head, slice, pipe, curry } from "ramda"
+import * as PROD from "react/jsx-runtime"
+import rehypeReact from "rehype-react"
 
-const utf8 = (x) => fs.promises.readFile(x, "utf8");
+const utf8 = (x) => fs.promises.readFile(x, "utf8")
 
-const readFile = encaseP(utf8);
+const readFile = encaseP(utf8)
 
 const trace = curry((msg, x) => {
-  console.log(msg, x);
-  return x;
-});
+  console.log(msg, x)
+  return x
+})
 
 const pickaxe = (x) =>
-  unified()
+  remark()
     .use(remarkParse)
-    // .use(remarkUTF8)
     .use(remarkObsidian)
+    .use(remarkBreaks)
+    // .use(remarkUTF8)
+    //.use(remarkGfm)
+    //.use(remarkLinks)
     .use(remarkFrontmatter, ["yaml"])
     .use(remarkParseFrontmatter)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeRaw, { passThrough: nodeTypes })
+    //.use(remarkRehype, { allowDangerousHtml: true })
+    //.use(rehypeRaw, { passThrough: nodeTypes })
     // .use(rehypeHighlight)
-    .use(rehypeFormat)
-    .use(rehypeStringify, { allowDangerousHtml: true })
+    //.use(rehypeFormat)
+    //.use(rehypeStringify, { allowDangerousHtml: true })
     // .use(rehypeReact, PROD)
-    .process(x);
+    .process(x)
 
-const cleanName = pipe((x) => x.slice(x.lastIndexOf("/"), -3), slug);
+const cleanName = pipe((x) => x.slice(x.lastIndexOf("/"), -3), slug)
 
-const format = (x) => prettier.format(x, { semi: false, parser: "typescript" });
-const prettify = encaseP(format);
+const format = (x) => prettier.format(x, { semi: false, parser: "typescript" })
+const prettify = encaseP(format)
 // const spotFix = replace(/\>\>/g, ">&gt;");
 
 const jsxify = curry((name, raw) => {
@@ -68,42 +75,42 @@ export const COMPONENT = () => {
 }
 
 export default COMPONENT
-`;
-});
+`
+})
 
-const fixNewlines = replace(/\n/g, "");
+//const fixNewlines = replace(/\n/g, "")
 const spotFix = pipe(
   replace(/=>/g, "=&gt;"),
   replace(/>/g, "&gt;"),
   // fixNewlines,
-);
-const fixClassNames = replace(/class=/g, "className=");
-const fixEntities = replace(/&#x26;gt;/g, "&gt;");
-const untransformNewlines = replace(/\n/g, "\n\n");
+)
+const fixClassNames = replace(/class=/g, "className=")
+const fixEntities = replace(/&#x26;gt;/g, "&gt;")
+//const untransformNewlines = replace(/\n/g, "\n\n")
 
 const postfix = pipe(
   fixClassNames,
   fixEntities,
   // replace(/\<pre\>\<code\>/g, "<pre><code>{`"),
   // replace(/\<\/pre>\<\/code\>/g, "`}</pre></code>"),
-);
+)
 
 const readObsidian = (raw) =>
   pipe(
     readFile,
-    map(spotFix),
+    //map(spotFix),
     chain(encaseP(pickaxe)),
-    map(jsxify(raw)),
-    map(postfix),
-    // chain(prettify),
-  )(raw);
+    //map(jsxify(raw)),
+    //map(postfix),
+    //chain(prettify),
+  )(raw)
 
-const j2 = (x) => JSON.stringify(x, null, 2);
-const stringifyFrontmatter = pipe(path(["data", "frontmatter"]), j2);
+const j2 = (x) => JSON.stringify(x, null, 2)
+const stringifyFrontmatter = pipe(path(["data", "frontmatter"]), j2)
 
 pipe(
   slice(2, Infinity),
   head,
   readObsidian,
   fork(console.warn)(console.log),
-)(process.argv);
+)(process.argv)
